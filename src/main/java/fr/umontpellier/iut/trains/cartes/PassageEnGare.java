f94ce2042b5c0d927378d6e21bd2cd3cfabdc4d9
michielk <kelian.michiel@etu.umontpellier.fr>
2024-04-09 16:10:44 +0200

    passage en gare passe test

--- src/main/java/fr/umontpellier/iut/trains/cartes/PassageEnGare.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/PassageEnGare.java
@@ -1,7 +1,14 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Joueur;
+
 public class PassageEnGare extends CarteRouge {
     public PassageEnGare() {
         super("Passage en gare",3,1);
+    }
+
+    @Override
+    public void jouer(Joueur joueur) {
+        joueur.getMain().add(joueur.piocher());
     }
 }
