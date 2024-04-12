a527896106e715c53b93123cbca4b031495ea62b
michielk <kelian.michiel@etu.umontpellier.fr>
2024-04-12 10:08:06 +0200

    tgv + salle de controle  passe tests

--- src/main/java/fr/umontpellier/iut/trains/cartes/TGV.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/TGV.java
@@ -1,7 +1,18 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Joueur;
+
 public class TGV extends CarteBleu {
     public TGV() {
         super("TGV",2,1);
+    }
+
+    @Override
+    public void jouer(Joueur joueur) {
+        if(joueur.getCartesEnJeu().getCarte("Train omnibus") != null)
+        {
+            joueur.addArgent(1);
+        }
+        joueur.getJeu().log(String.format("%s reçoit 1 d'argent", joueur.getNom()));
     }
 }
