c26c1d2b87e35220613615eaa34396004e38739b
michielk <kelian.michiel@etu.umontpellier.fr>
2024-04-17 15:37:27 +0200

    train postal passe test

--- src/main/java/fr/umontpellier/iut/trains/cartes/TrainPostal.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/TrainPostal.java
@@ -1,7 +1,26 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Joueur;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrainPostal extends CarteBleu {
     public TrainPostal() {
         super("Train postal",4,1);
+    }
+
+    @Override
+    public void jouer(Joueur joueur) {
+        List<String> choixPossibles = joueur.getNomsCartes(joueur.getMain());
+        String instructions = "choisissez une carte à défausser de votre main ou passez pour terminer l'effet de la carte";
+        String choix = joueur.choisir(instructions,choixPossibles,null,true);
+        while(!choix.isEmpty())
+        {
+            joueur.getDefausse().add(joueur.getMain().retirer(choix));
+            joueur.addArgent(1);
+            choix = joueur.choisir(instructions,choixPossibles,null,true);
+        }
+
     }
 }
