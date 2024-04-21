6b08cfec647bc5f8f33184e739db12ce3ab04f07
challiase <evan.challias@etu.umontpellier.fr>
2024-04-21 18:12:49 +0200

    probleme bizarre su depot a revoir

--- src/main/java/fr/umontpellier/iut/trains/cartes/Depot.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/Depot.java
@@ -1,5 +1,7 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import ch.qos.logback.core.joran.action.NewRuleAction;
+import fr.umontpellier.iut.trains.Bouton;
 import fr.umontpellier.iut.trains.Joueur;
 
 import java.util.ArrayList;
@@ -12,36 +14,33 @@ public class Depot extends CarteRouge {
 
     public void joueur (Joueur joueur)
     {
-        // Piocher automatiquement deux cartes
-        List<Carte> cartesPiochees = joueur.piocher(2);
 
-        // Si le joueur a strictement moins de 2 cartes en main, défausser ce qu'il a et terminer l'exécution de la carte
+        joueur.getMain().add(joueur.piocher());
+        joueur.getMain().add(joueur.piocher());
+
+        /*List<Bouton> listeChoix = new ArrayList<>();
+        for (Carte c : joueur.getMain())
+        {
+            listeChoix.add(new Bouton(c.getNom()));
+        }
+
+        for (int i = 0; i < 2; i++)
+        {
+            String input = joueur.choisir("Qelle carte souhaitez vous defaussez ? ", null, listeChoix, false);
+            joueur.defausserCarte(joueur.getMain().getCarte(input));
+        }
+*/
+
+
+        /*je l'ai mis, mais la consigne est bizarre
         if (joueur.getMain().size() < 2) {
             joueur.getDefausse().addAll(joueur.getMain());
             joueur.getMain().clear();
             return;
-        }
+        }*/
 
-        // Afficher les cartes piochées
-        joueur.log("Vous avez pioché : " + cartesPiochees.get(0) + " et " + cartesPiochees.get(1));
 
-        // Demander au joueur de choisir deux cartes à défausser parmi celles qu'il a en main
-        List<String> choixPossibles = joueur.getNomMain();
-        List<String> cartesDefausser = new ArrayList<>();
-        for (int i = 0; i < 2; i++) {
-            // Demander au joueur de choisir une carte à défausser
-            String choix = joueur.choisir("Choisissez une carte à défausser :", choixPossibles, null, false);
-            // Ajouter la carte choisie à la liste des cartes à défausser
-            cartesDefausser.add(choix);
-            // Retirer la carte choisie de la liste des choix possibles
-            choixPossibles.remove(choix);
-        }
 
-        // Défausser les cartes choisies par le joueur
-        for (String nomCarte : cartesDefausser) {
-            Carte carteDefausser = joueur.getMain().retirer(nomCarte);
-            joueur.defausserCarte(carteDefausser);
-        }
 
     }
 }
